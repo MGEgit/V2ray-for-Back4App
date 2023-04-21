9b79c1d727be26dfa5c100194b9c6ed5a0323829
MGEgit <99439778+MGEgit@users.noreply.github.com>
2023-04-22 00:54:31 +0330

    ID

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # Define UUID and masquerade path, please modify it yourself. (Note: The masquerading path starts with / symbol, in order to avoid unnecessary trouble, please do not use special symbols.)
-UUID=${UUID:-'db5b4014-d2da-11ed-afa1-0242ac120002'}
-VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
-VLESS_WSPATH=${VLESS_WSPATH:-'/vless'}
-TROJAN_WSPATH=${TROJAN_WSPATH:-'/trojan'}
-SS_WSPATH=${SS_WSPATH:-'/shadowsocks'}
+UUID=${UUID:-'8e2bd766-2b09-400a-9819-78763f44a266'}
+VMESS_WSPATH=${VMESS_WSPATH:-'/vv'}
+VLESS_WSPATH=${VLESS_WSPATH:-'/ll'}
+TROJAN_WSPATH=${TROJAN_WSPATH:-'/tt'}
+SS_WSPATH=${SS_WSPATH:-'/ss'}
 sed -i "s#UUID#$UUID#g;s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g;s#TROJAN_WSPATH#${TROJAN_WSPATH}#g;s#SS_WSPATH#${SS_WSPATH}#g" config.json
 sed -i "s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g;s#TROJAN_WSPATH#${TROJAN_WSPATH}#g;s#SS_WSPATH#${SS_WSPATH}#g" /etc/nginx/nginx.conf
 
